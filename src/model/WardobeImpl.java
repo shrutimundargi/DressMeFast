00003c9316c7891330dfee542c5054c9ef1060c9
contidnb <jacopo.conti2@studio.unibo.it>
2017-04-19 10:20:29 +0200

    Modificate classi Category e CategoryManagement

--- src/model/WardobeImpl.java
+++ src/model/WardobeImpl.java
@@ -1,7 +1,23 @@
 package model;
 
+import java.util.Map;
+
+import model.interfaces.Category;
+import model.interfaces.CategoryManagement;
 import model.interfaces.Wardrobe;
 
 public class WardobeImpl implements Wardrobe {
+    
+    private CategoryManagement categoryManagement;
+    
+    public WardobeImpl() {
+        
+    }
 
+    @Override
+    public CategoryManagement getAllCategories() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
