40e123b34105de3659f21df15ba21c9bb9dc4513
carlo <carlo.noia@studio.unibo.it>
2017-04-26 14:19:10 +0200

    Aggiornata javadoc

--- src/controller/authentication/Authentication.java
+++ src/controller/authentication/Authentication.java
@@ -15,8 +15,11 @@ public interface Authentication {
 	 *            username
 	 * @param pass
 	 *            password
-	 * @return the status of the Login operation. <p>Return USER_NOT_FOUND if the
-	 *         username don't exist, and WORONG_PASSWORD if the password is wrong
+	 * @return the status of the Login operation.
+	 *         <p>
+	 *         Return USER_NOT_FOUND if the username don't exist, and
+	 *         WORONG_PASSWORD if the password is wrong, otherwise if everything
+	 *         goes well return USER_FOUND
 	 * 
 	 */
 	Status checkLogin(String user, String pass);
@@ -26,17 +29,20 @@ public interface Authentication {
 	 *            username
 	 * @param pass
 	 *            password
-	 * @return the status of the signUp operation. <P>Return USERNAME_ALREADY_TAKEN
+	 * @return the status of the signUp operation.
+	 *         <P>
+	 *         Return USERNAME_ALREADY_TAKEN if the username already exist
+	 *         otherwise if everything goes well return USER_REGISTERED
 	 */
 	Status signUp(String user, String pass);
 
 	/**
-	 * @return the status of the logout operation
+	 * @return the status of the logout operation <P>Return LOGOUT_SUCCESFULL
 	 */
 	Status logout();
 
 	/**
-	 * @return the username of the User
+	 * @return the username of the User 
 	 */
 	String getUsername();
 
