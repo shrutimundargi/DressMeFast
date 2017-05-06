f61226afcacece2a9643b80418c1bfbc9d508ef6
Jacopo Conti <jacopo.conti2@studio.unibo.it>
2017-05-06 15:22:35 +0200

    Refactor of outfits classes/interfaces

--- src/model/classes/AIOutfit.java
+++ src/model/classes/AIOutfit.java
@@ -1,7 +1,11 @@
 package model.classes;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import model.enumerations.Status;
 import model.interfaces.Outfits;
 
 /**
@@ -10,10 +14,42 @@ import model.interfaces.Outfits;
  */
 public class AIOutfit implements Outfits {
 
+    private final UUID id;
+    private Optional<String> name;
+    private List<UUID> outfit;
+    // private final Set<CategoryManagement> categoryM;
+
+    /**
+     * Creates a container of a single AI outfit.
+     */
+    public AIOutfit() {
+        this.id = UUID.randomUUID();
+        this.name = Optional.empty();
+        this.outfit = new LinkedList<>();
+    }
     @Override
     public UUID getId() {
+        return this.id;
+    }
+    @Override
+    public List<UUID> getOutfit() {
+        return this.outfit;
+    }
+    @Override
+    public Optional<String> getName() {
+        return this.name;
+    }
+
+    @Override
+    public Outfits createOutfit(final List<UUID> dressList) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public Status setName(final String name) {
+        this.name = Optional.ofNullable(name);
+        return Status.CHANGE_SUCCESFULL;
     }
 
 }
