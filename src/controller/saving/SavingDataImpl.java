d79094111411f10d8ab95b23bcaf8b439242810f
U-DESKTOP-235KCBP\Carlo <carlo.noia@studio.unibo.it>
2017-05-09 11:43:46 +0200

    Added methods for loading and saving data

---
+++ src/controller/saving/SavingDataImpl.java
@@ -0,0 +1,24 @@
+package controller.saving;
+
+import model.enumerations.Status;
+import model.interfaces.User;
+
+/**
+ *  An implementation of the SavingData.
+ *
+ */
+public class SavingDataImpl implements SavingData {
+
+    @Override
+    public Status save(final User user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Status load() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
