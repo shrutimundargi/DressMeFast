63e6233529123d4462214d85bea96be32abde0ee
contidnb <jacopo.conti2@studio.unibo.it>
2017-05-31 10:25:33 +0200

    Fixed bug when generating an outfit.

--- src/model/classes/AIOutfit.java
+++ src/model/classes/AIOutfit.java
@@ -181,25 +181,25 @@ public class AIOutfit implements Outfits {
                 this.outfit.add(randomFootDress.getId());
             }
         }
-
+        /*
         if (!dressBody.isEmpty() && dressHands.isEmpty() && dressHead.isEmpty() && dressLegs.isEmpty()
                 && dressNeck.isEmpty() && dressFoot.isEmpty()) {
             final Dress randomBodyDressTmp = dressBody.get(randomizer.nextInt(dressBody.size()));
             this.outfit.add(randomBodyDressTmp.getId());
         }
-
+        */
         if (dressBody.isEmpty() && !dressHands.isEmpty() && dressHead.isEmpty() && dressLegs.isEmpty()
                 && dressNeck.isEmpty() && dressFoot.isEmpty()) {
             final Dress randomHandsDressTmp = dressHands.get(randomizer.nextInt(dressHands.size()));
             this.outfit.add(randomHandsDressTmp.getId());
         }
-
+        /*
         if (dressBody.isEmpty() && dressHands.isEmpty() && !dressHead.isEmpty() && dressLegs.isEmpty()
                 && dressNeck.isEmpty() && dressFoot.isEmpty()) {
             final Dress randomHeadDressTmp = dressHead.get(randomizer.nextInt(dressHead.size()));
             this.outfit.add(randomHeadDressTmp.getId());
         }
-
+        */
         if (dressBody.isEmpty() && dressHands.isEmpty() && dressHead.isEmpty() && !dressLegs.isEmpty()
                 && dressNeck.isEmpty() && dressFoot.isEmpty()) {
             final Dress randomLegsDressTmp = dressLegs.get(randomizer.nextInt(dressLegs.size()));
